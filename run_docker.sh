450aa9452467ada5b234b357c6f4e2c637ed0c20
adegokem <adegokefoluso@gmail.com>
2022-06-11 23:32:29 -0400

    Removing Error

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,9 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=capstone .
+IMAGE=fastapi
+
+docker build --tag=$IMAGE .
 
 # Step 2: 
 # List docker images
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,7 +5,7 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=adegokem/flaskapp
+dockerpath=adegokem/capstone
 
 # Step 2:  
 # Authenticate & tag
