ca599663bfcf893f8d533338ad6ba0d7c16fac13
adegokem <adegokefoluso@gmail.com>
2022-07-04 23:07:20 -0400

    add screenhots

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return '<h1>This is my Capstone Project DevOps Udacity, \
-            my name is Mofoluwaso.</h1>'
+            My Name is Mofoluwaso.</h1>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
